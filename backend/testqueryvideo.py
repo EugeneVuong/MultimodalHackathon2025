4c351ae61e2e8dc7126ea7bc81f273dc21cdd3c5
Nicholas Ferreira <nicholasjferreira98@gmail.com>
2025-02-15 23:43:42 -0800

    Fixed authentication errors

---
+++ backend/testqueryvideo.py
@@ -0,0 +1,15 @@
+import json
+import os
+from aperturedb import Connector as Connector
+from aperturedb.CommonLibrary import create_connector
+from dotenv import load_dotenv
+from apertureconnection import add_video_to_aperture, search_videos_by_caption, search_video_by_id  # Assuming your function is in video_ingestion.py
+
+
+user_search = "test"
+results = search_videos_by_caption(user_search)
+print("Search Results:", results)
+
+# search_by_id = 1
+# result = search_video_by_id(search_by_id)
+# print("Search Results:", result)
